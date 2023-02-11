10be84ee6139ccc9452c10d9ef166cf09306c47c
sufiyanmalek <sufiyanmalek.geek@gmail.com>
2023-02-11 14:27:05 +0530

    Day2 Assignment 4 completed

--- CSS/Day2/Assignments/Assignment4/assignment4/main.js
+++ CSS/Day2/Assignments/Assignment4/assignment4/main.js
@@ -1,1 +1,23 @@
 import './style.css'
+
+let d3assign1 = document.getElementById('day3-assign1')
+d3assign1.onclick = showd3assign1;
+
+function showd3assign1 ()
+{
+    let loc = "./Employee Form/index.html";
+    document.getElementById('web-page').src = loc;
+}
+
+
+let d3assign2 = document.getElementById('day3-assign2');
+d3assign2.onclick = showd3assign2;
+
+function showd3assign2 ()
+{
+    let loc = "./Resume/index.html";
+    document.getElementById('web-page').src = loc;
+}
+
+
+
